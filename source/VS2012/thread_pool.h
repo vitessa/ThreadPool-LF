4d4c8cda1f4f65aa4f66e99f9dad570181fd78c1
Vitessa <51199696+Vitessa@users.noreply.github.com>
2019-12-25 16:43:08 +0800

    style: 调整格式

--- source/VS2012/thread_pool.h
+++ source/VS2012/thread_pool.h
@@ -23,86 +23,89 @@
 class CThreadPool
 {
 public:
-	CThreadPool() : _isStop(false)
-	{
-		_vecThread.emplace_back(CThreadPool::route, this);
-	}
-	CThreadPool(int thread_num) : _isStop(false)
-	{
-		for (int i=0; i<thread_num; ++i) {
-			_vecThread.emplace_back(CThreadPool::route, this);
-		}
-	}
-	~CThreadPool()
-	{
-		{
-			std::unique_lock<std::mutex> lock(_mtx);
-			_isStop = true;
-		}
-		_condFollower.notify_all();
-		_condLeader.notify_all();
-		for (std::thread& t : _vecThread) {
-			t.join();
-		}
-	}
+    CThreadPool() : _isStop(false)
+    {
+        _vecThread.emplace_back(CThreadPool::route, this);
+    }
+    CThreadPool(int thread_num) : _isStop(false)
+    {
+        for (int i=0; i<thread_num; ++i) {
+            _vecThread.emplace_back(CThreadPool::route, this);
+        }
+    }
+    ~CThreadPool()
+    {
+        {
+            std::unique_lock<std::mutex> lock(_mtx);
+            _isStop = true;
+        }
+        _condFollower.notify_all();
+        _condLeader.notify_all();
+        for (std::thread& t : _vecThread) {
+            t.join();
+        }
+    }
 
-	template<typename Fn>
-	static std::future<typename std::result_of<Fn()>::type> spawn(CThreadPool& tp, Fn func)
-	{
-		typedef std::result_of<Fn()>::type rt_type;
+    template<typename Fn>
+    static std::future<typename std::result_of<Fn()>::type> spawn(CThreadPool& tp, Fn func)
+    {
+        typedef std::result_of<Fn()>::type rt_type;
 
-		auto task = std::make_shared<std::packaged_task<rt_type()>>(std::forward<Fn>(func));
-		std::future<rt_type> res = task->get_future();
+        auto task = std::make_shared<std::packaged_task<rt_type()>>(std::forward<Fn>(func));
+        std::future<rt_type> res = task->get_future();
 
-		{
-			std::unique_lock<std::mutex> lock(tp._mtx);
-			if (tp._isStop) {
-				throw std::runtime_error("spawn a stoped thread pool.");
-			}
-			tp._queTask.emplace([task](){(*task)();});
-		}
+        {
+            std::unique_lock<std::mutex> lock(tp._mtx);
+            if (tp._isStop) {
+                throw std::runtime_error("spawn a stoped thread pool.");
+            }
+            tp._queTask.emplace([task](){(*task)();});
+        }
 
-		tp._condLeader.notify_one();
-		return res;
-	}
+        tp._condLeader.notify_one();
+        return res;
+    }
+
 private:
-	CThreadPool(const CThreadPool&){}
-	static void route(CThreadPool* tp)
-	{
-		std::function<void()> task;
-		for (;;)
-		{
-			{   // lock_guard start
-				std::unique_lock<std::mutex> lock(tp->_mtx);
-				if (tp->_leaderId == std::thread::id()) {
-					// Leader
-					tp->_leaderId = std::this_thread::get_id();
-					tp->_condLeader.wait(lock, [tp]{return tp->_isStop || !tp->_queTask.empty();});
-					if (tp->_queTask.empty()) { return; }
-					tp->_leaderId = std::thread::id();
-					task = std::move(tp->_queTask.front());
-					tp->_queTask.pop();
-				}
-				else {
-					// Follower
-					tp->_condFollower.wait(lock, [tp]{return tp->_isStop || tp->_leaderId==std::thread::id();});
-					if (tp->_isStop && tp->_queTask.empty()) {return;}
-					continue;
-				}
-			}   // lock_guard end
+    CThreadPool(const CThreadPool&){}
 
-			// Call Follower to become Leader
-			tp->_condFollower.notify_one();
-			// Worker
-			task();
-		}
-	}
+    static void route(CThreadPool* tp)
+    {
+        std::function<void()> task;
+        for (;;)
+        {
+            {   // lock_guard start
+                std::unique_lock<std::mutex> lock(tp->_mtx);
+                if (tp->_leaderId == std::thread::id()) {
+                    // Leader
+                    tp->_leaderId = std::this_thread::get_id();
+                    tp->_condLeader.wait(lock, [tp]{return tp->_isStop || !tp->_queTask.empty();});
+                    if (tp->_queTask.empty()) { return; }
+                    tp->_leaderId = std::thread::id();
+                    task = std::move(tp->_queTask.front());
+                    tp->_queTask.pop();
+                }
+                else {
+                    // Follower
+                    tp->_condFollower.wait(lock, [tp]{return tp->_isStop || tp->_leaderId==std::thread::id();});
+                    if (tp->_isStop && tp->_queTask.empty()) {return;}
+                    continue;
+                }
+            }   // lock_guard end
+
+            // Call Follower to become Leader
+            tp->_condFollower.notify_one();
+            // Worker
+            task();
+        }
+    }
+
 private:
-	std::vector<std::thread> _vecThread;		// 线程组
-	std::queue<std::function<void()>> _queTask;	// 任务队列
-	std::mutex _mtx;							// 互斥锁
-	bool _isStop;								// 是否停止（析构函数用）
-	std::condition_variable _condFollower;
-	std::condition_variable _condLeader;
-	std::thread::id _leaderId;
+    std::vector<std::thread> _vecThread;        // 线程组
+    std::queue<std::function<void()>> _queTask; // 任务队列
+    std::mutex _mtx;                            // 互斥锁
+    bool _isStop;                                // 是否停止（析构函数用）
+    std::condition_variable _condFollower;
+    std::condition_variable _condLeader;
+    std::thread::id _leaderId;
 };
